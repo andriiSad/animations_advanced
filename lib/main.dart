b3bd53de3a47c7ceeca9430cc7a99100c6f080a3
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-03-31 13:47:55 +0200

    zooming implicit animation image

--- lib/main.dart
+++ lib/main.dart
@@ -5,150 +5,68 @@ void main() {
     MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
       ),
-      darkTheme: ThemeData.dark(),
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
     ),
   );
 }
 
-@immutable
-class Person {
-  final String name;
-  final int age;
-  final String emoji;
-
-  const Person({
-    required this.name,
-    required this.age,
-    required this.emoji,
-  });
-}
-
-const people = [
-  Person(name: "John", age: 20, emoji: '👽'),
-  Person(name: "Jane", age: 21, emoji: '🤖'),
-  Person(name: "Jack", age: 22, emoji: '🎃'),
-];
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('People'),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        itemCount: people.length,
-        itemBuilder: (context, index) {
-          final person = people[index];
-          return ListTile(
-            leading: Hero(
-              tag: person.name,
-              child: Material(
-                color: Colors.transparent,
-                child: Text(
-                  person.emoji,
-                  style: const TextStyle(fontSize: 30),
-                ),
-              ),
-            ),
-            title: Text(person.name),
-            subtitle: Text("${person.age} years old"),
-            trailing: const Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DetailsPage(
-                    person: person,
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
+  State<HomePage> createState() => _HomePageState();
 }
 
-class DetailsPage extends StatelessWidget {
-  const DetailsPage({
-    super.key,
-    required this.person,
-  });
+const defaultWidth = 100.0;
 
-  final Person person;
+class _HomePageState extends State<HomePage> {
+  var _isZoomedIn = false;
+  var _buttonTitle = 'Zoom In';
+  var _width = defaultWidth;
+  var _curve = Curves.bounceOut;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Hero(
-          flightShuttleBuilder: (
-            flightContext,
-            animation,
-            flightDirection,
-            fromHeroContext,
-            toHeroContext,
-          ) {
-            switch (flightDirection) {
-              case HeroFlightDirection.push:
-                return Material(
-                  color: Colors.transparent,
-                  child: ScaleTransition(
-                    scale: animation.drive(
-                      Tween<double>(
-                        begin: 0,
-                        end: 1,
-                      ).chain(
-                        CurveTween(
-                          curve: Curves.fastOutSlowIn,
-                        ),
-                      ),
-                    ),
-                    child: toHeroContext.widget,
-                  ),
-                );
-              case HeroFlightDirection.pop:
-                return Material(
-                  color: Colors.transparent,
-                  child: fromHeroContext.widget,
-                );
-            }
-          },
-          tag: person.name,
-          child: Text(
-            person.emoji,
-            style: const TextStyle(fontSize: 30),
-          ),
-        ),
-        centerTitle: true,
+        title: const Text('Home Page'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              person.name,
-              style: const TextStyle(fontSize: 25),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "${person.age} years old",
-              style: const TextStyle(fontSize: 25),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                width: _width,
+                duration: const Duration(milliseconds: 370),
+                curve: _curve,
+                child: Image.asset(
+                  'assets/images/wallpaper.png',
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: () {
+              setState(
+                () {
+                  _isZoomedIn = !_isZoomedIn;
+                  _buttonTitle = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
+                  _width = _isZoomedIn
+                      ? MediaQuery.of(context).size.width
+                      : defaultWidth;
+                  _curve = _isZoomedIn ? Curves.bounceInOut : Curves.bounceOut;
+                },
+              );
+            },
+            child: Text(_buttonTitle),
+          ),
+        ],
       ),
     );
   }
