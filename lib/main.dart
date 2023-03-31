fe1b4d2bcd91bc59ed23372bdc8bf56d615c71cb
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-03-31 15:04:55 +0200

    changing colors circle, using TweenAnimationBuilder and CustomClipper

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(
@@ -13,6 +14,35 @@ void main() {
   );
 }
 
+class CircleClipper extends CustomClipper<Path> {
+  const CircleClipper();
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+
+    final rect = Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: size.width / 2,
+    );
+
+    path.addOval(rect);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+Color getRandomColor() {
+  return Color.fromARGB(
+    255,
+    math.Random().nextInt(256),
+    math.Random().nextInt(256),
+    math.Random().nextInt(256),
+  );
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -20,53 +50,42 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-const defaultWidth = 100.0;
-
 class _HomePageState extends State<HomePage> {
-  var _isZoomedIn = false;
-  var _buttonTitle = 'Zoom In';
-  var _width = defaultWidth;
-  var _curve = Curves.bounceOut;
+  var _color = getRandomColor();
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedContainer(
-                width: _width,
-                duration: const Duration(milliseconds: 370),
-                curve: _curve,
-                child: Image.asset(
-                  'assets/images/wallpaper.png',
+      body: Center(
+        child: ClipPath(
+          clipper: const CircleClipper(),
+          child: TweenAnimationBuilder(
+            tween: ColorTween(
+              begin: getRandomColor(),
+              end: _color,
+            ),
+            onEnd: () {
+              setState(() {
+                _color = getRandomColor();
+              });
+            },
+            duration: const Duration(seconds: 1),
+            builder: (context, color, child) {
+              return ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  color!,
+                  BlendMode.srcATop,
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: () {
-              setState(
-                () {
-                  _isZoomedIn = !_isZoomedIn;
-                  _buttonTitle = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
-                  _width = _isZoomedIn
-                      ? MediaQuery.of(context).size.width
-                      : defaultWidth;
-                  _curve = _isZoomedIn ? Curves.bounceInOut : Curves.bounceOut;
-                },
+                child: Container(
+                  width: width,
+                  height: width,
+                  color: getRandomColor(),
+                ),
               );
             },
-            child: Text(_buttonTitle),
           ),
-        ],
+        ),
       ),
     );
   }
