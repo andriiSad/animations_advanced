12479a13dd5168c25ffff7d8dec6b02493f8b2fe
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-03-31 11:37:12 +0200

    hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,174 +1,151 @@
-import 'dart:math' show pi;
-
 import 'package:flutter/material.dart';
-import 'package:vector_math/vector_math_64.dart' show Vector3;
 
 void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(brightness: Brightness.light),
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      darkTheme: ThemeData.dark(),
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
-      debugShowMaterialGrid: false,
     ),
   );
 }
 
-const widthAndHeight = 100.0;
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String emoji;
 
-class HomePage extends StatefulWidget {
+  const Person({
+    required this.name,
+    required this.age,
+    required this.emoji,
+  });
+}
+
+const people = [
+  Person(name: "John", age: 20, emoji: '👽'),
+  Person(name: "Jane", age: 21, emoji: '🤖'),
+  Person(name: "Jack", age: 22, emoji: '🎃'),
+];
+
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('People'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          final person = people[index];
+          return ListTile(
+            leading: Hero(
+              tag: person.name,
+              child: Material(
+                color: Colors.transparent,
+                child: Text(
+                  person.emoji,
+                  style: const TextStyle(fontSize: 30),
+                ),
+              ),
+            ),
+            title: Text(person.name),
+            subtitle: Text("${person.age} years old"),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DetailsPage(
+                    person: person,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  late AnimationController _xController;
-  late AnimationController _yController;
-  late AnimationController _zController;
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({
+    super.key,
+    required this.person,
+  });
 
-  late Tween<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _xController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 20),
-    );
-
-    _yController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 30),
-    );
-
-    _zController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 40),
-    );
-
-    _animation = Tween<double>(
-      begin: 0,
-      end: pi * 2,
-    );
-  }
-
-  @override
-  void dispose() {
-    _xController.dispose();
-    _yController.dispose();
-    _zController.dispose();
-    super.dispose();
-  }
+  final Person person;
 
   @override
   Widget build(BuildContext context) {
-    _xController
-      ..reset()
-      ..repeat();
-
-    _yController
-      ..reset()
-      ..repeat();
-
-    _zController
-      ..reset()
-      ..repeat();
-
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 100,
-              width: double.infinity,
-            ),
-            AnimatedBuilder(
-              animation: Listenable.merge(
-                [
-                  _xController,
-                  _yController,
-                  _zController,
-                ],
-              ),
-              builder: (context, child) {
-                return Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.identity()
-                    ..rotateX(_animation.evaluate(_xController))
-                    ..rotateY(_animation.evaluate(_yController))
-                    ..rotateZ(_animation.evaluate(_zController)),
-                  child: Stack(
-                    children: [
-                      //back
-                      Transform(
-                        alignment: Alignment.center,
-                        transform: Matrix4.identity()
-                          ..translate(
-                            Vector3(0, 0, -widthAndHeight),
-                          ),
-                        child: Container(
-                          color: Colors.purple,
-                          width: widthAndHeight,
-                          height: widthAndHeight,
+      appBar: AppBar(
+        title: Hero(
+          flightShuttleBuilder: (
+            flightContext,
+            animation,
+            flightDirection,
+            fromHeroContext,
+            toHeroContext,
+          ) {
+            switch (flightDirection) {
+              case HeroFlightDirection.push:
+                return Material(
+                  color: Colors.transparent,
+                  child: ScaleTransition(
+                    scale: animation.drive(
+                      Tween<double>(
+                        begin: 0,
+                        end: 1,
+                      ).chain(
+                        CurveTween(
+                          curve: Curves.fastOutSlowIn,
                         ),
                       ),
-                      //left side
-                      Transform(
-                        alignment: Alignment.centerLeft,
-                        transform: Matrix4.identity()..rotateY(pi / 2),
-                        child: Container(
-                          color: Colors.red,
-                          width: widthAndHeight,
-                          height: widthAndHeight,
-                        ),
-                      ),
-                      //right side
-                      Transform(
-                        alignment: Alignment.centerRight,
-                        transform: Matrix4.identity()..rotateY(-pi / 2),
-                        child: Container(
-                          color: Colors.blue,
-                          width: widthAndHeight,
-                          height: widthAndHeight,
-                        ),
-                      ),
-                      //front
-                      Container(
-                        color: Colors.green,
-                        width: widthAndHeight,
-                        height: widthAndHeight,
-                      ),
-                      //top side
-                      Transform(
-                        alignment: Alignment.topCenter,
-                        transform: Matrix4.identity()..rotateX(-pi / 2),
-                        child: Container(
-                          color: Colors.orange,
-                          width: widthAndHeight,
-                          height: widthAndHeight,
-                        ),
-                      ),
-                      //bottom side
-                      Transform(
-                        alignment: Alignment.bottomCenter,
-                        transform: Matrix4.identity()..rotateX(pi / 2),
-                        child: Container(
-                          color: Colors.brown,
-                          width: widthAndHeight,
-                          height: widthAndHeight,
-                        ),
-                      ),
-                    ],
+                    ),
+                    child: toHeroContext.widget,
                   ),
                 );
-              },
+              case HeroFlightDirection.pop:
+                return Material(
+                  color: Colors.transparent,
+                  child: fromHeroContext.widget,
+                );
+            }
+          },
+          tag: person.name,
+          child: Text(
+            person.emoji,
+            style: const TextStyle(fontSize: 30),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              person.name,
+              style: const TextStyle(fontSize: 25),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "${person.age} years old",
+              style: const TextStyle(fontSize: 25),
             ),
           ],
         ),
